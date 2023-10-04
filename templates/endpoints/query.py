4af9c48631321bc05071f143301ea3c48a95297e
mjconnor <mattc@anyscale.com>
2023-10-03 19:52:58 -0700

    updates

---
+++ templates/endpoints/query.py
@@ -0,0 +1,12 @@
+import openai
+
+# import aviary.backend.router_application
+
+# Note: not all arguments are currently supported and will be ignored by the backend.
+chat_completion = openai.ChatCompletion.create(
+    api_base="http://localhost:8000/v1", api_key="",
+    model="meta-llama/Llama-2-7b-chat-hf",
+    messages=[{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": "What are some of the best restaurants in San Francisco?'."}],
+    temperature=0.01
+)
+print(chat_completion)
