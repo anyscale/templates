7716cad380e4f7d594c2e630ff7d00257ac0c2ec
Huaiwei Sun <scottsun94@github.com>
2024-01-09 18:07:20 -0800

    address comments

--- templates/serve-stable-diffusion/app.py
+++ templates/serve-stable-diffusion/app.py
@@ -5,13 +5,17 @@ from fastapi import FastAPI
 from fastapi.responses import Response
 import torch
 from diffusers import EulerDiscreteScheduler, StableDiffusionPipeline
+import logging
 
 # Create a FastAPI instance. Ray Serve has integration with FastAPI for complex HTTP handling logic. Learn more: https://docs.ray.io/en/latest/serve/http-guide.html#fastapi-http-deployments
 app = FastAPI()
 
+# Set up the Ray Serve logger
+logger = logging.getLogger("ray.serve")
+
 # A serve deployment contains business logic or an ML model to handle incoming requests.
 # It consists of a number of replicas that are individual copies of the class or function defined within it.
-@serve.deployment(num_replicas=1, route_prefix="/")
+@serve.deployment(num_replicas=1)
 @serve.ingress(app)
 class APIIngress:
     def __init__(self, diffusion_model_handle) -> None:
@@ -34,12 +38,13 @@ class APIIngress:
 
 
 @serve.deployment(
-    # Set the number of GPUs required for each model replica
-    ray_actor_options={"num_gpus": 1},
-
-    # The number of model replicas to keep active in the Ray cluster. These model replicas can process incoming requests concurrently. 
-    # Autoscaling of model replicas is enabled below.
-    autoscaling_config={"min_replicas": 2, "max_replicas": 99},
+    ray_actor_options={"num_gpus": 1, "num_cpus": 1},  # Set the number of GPUs and CPUs required for each model replica.
+    max_concurrent_queries=2,   # Maximum number of queries that are sent to a replica of this deployment without receiving a response.
+    autoscaling_config={
+        "min_replicas": 1,    # The minimum number of model replicas to keep active in the Ray cluster.
+        "max_replicas": 99,    # The maximum number of model replicas to keep active in the Ray cluster.
+        "target_num_ongoing_requests_per_replica": 1,   # Target number of ongoing requests in a replica. Serve compares the actual number agasint this value and upscales or downscales.
+    },
 )
 class StableDiffusionV2:
     def __init__(self):
@@ -57,7 +62,8 @@ class StableDiffusionV2:
 
     def generate(self, prompt: str, img_size: int = 512):
         assert len(prompt), "prompt parameter cannot be empty"
-
+        # Use Serve logger to emit structured (JSON) logs as the best practice for production use cases.
+        logger.info("Prompt: [%s]", prompt)
         image = self.pipe(prompt, height=img_size, width=img_size).images[0]
 
         return image
