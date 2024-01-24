d9b92ad1ff4fab027053b8a679186f00b9a36a1b
akshay-anyscale <122416226+akshay-anyscale@users.noreply.github.com>
2024-01-24 11:27:48 -0800

    Update query.py

--- templates/endpoints_v2/query.py
+++ templates/endpoints_v2/query.py
@@ -3,8 +3,8 @@ from openai import OpenAI
 
 # Note: not all arguments are currently supported and will be ignored by the backend.
 client = OpenAI(
-    base_url="http://localhost:8000/v1",
-    api_key="NOT A REAL KEY",
+    base_url="http://localhost:8000/v1", # <- replace http://localhost:8000 with the Anyscale Service query url
+    api_key="NOT A REAL KEY", # <- replace with the bearer token if querying an Anyscale Service
 )
 chat_completion = client.chat.completions.create(
     model="mistralai/Mistral-7B-Instruct-v0.1",
