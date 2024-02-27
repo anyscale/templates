0ad3a606c6e3e4411cf0d88f35a6c5697e5af648
Brent Bain <brent@anyscale.com>
2024-02-27 09:02:42 -0800

    upd: Add HF_TOKEN as runtime env parameter
    Changes to be committed:
    	modified:   templates/batch-llm/batch_llm.ipynb
    	modified:   templates/batch-llm/batch_llm.py
    	modified:   templates/batch-llm/batch_llm_job.yaml

--- templates/batch-llm/batch_llm.py
+++ templates/batch-llm/batch_llm.py
@@ -4,20 +4,29 @@ import numpy as np
 import ray
 import os
 
+# Set the Hugging Face token. Replace the following with your token.
+hf_token = "<REPLACE_WITH_YOUR_HUGGING_FACE_USER_TOKEN>"
+# Set to the model that you wish to use. Note that using the llama models will require a hugging face token to be set.
+hf_model = "meta-llama/Llama-2-7b-chat-hf"
+# Create a sampling params object.
+sampling_params = SamplingParams(temperature=0, max_tokens=4096)
+
+
+# Initialize Ray with a Runtime Environment - used to set the Environment Variable `HF_TOKEN`
+# This line can be removed if setting the Environment Variable using a Cluster Environment or in the job yaml file.
+ray.init(runtime_env={"env_vars": {"HF_TOKEN": hf_token}})
+
 input_path = "s3://anonymous@air-example-data/prompts.txt"
 ds = ray.data.read_text(
     input_path,
     parallelism=10,
 )
 
-# Create a sampling params object.
-sampling_params = SamplingParams(temperature=0)
-
 
 class LLMPredictor:
     def __init__(self):
         # Create an LLM.
-        self.llm = LLM(model="meta-llama/Llama-2-7b-chat-hf")
+        self.llm = LLM(model=hf_model)
 
     def __call__(self, batch: Dict[str, np.ndarray]) -> Dict[str, list]:
         # Generate texts from the prompts. The output is a list of RequestOutput objects
