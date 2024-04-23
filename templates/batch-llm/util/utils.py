b2114c6ada2c183d26df844a11fe0fb14fe35c0b
Scott Lee <sjl@anyscale.com>
2024-04-23 11:04:36 -0700

    update hf token fetching
    Signed-off-by: Scott Lee <sjl@anyscale.com>

--- templates/batch-llm/util/utils.py
+++ templates/batch-llm/util/utils.py
@@ -3,6 +3,11 @@ import random
 import requests
 import string
 
+import huggingface_hub
+from transformers import AutoConfig
+
+HF_TOKEN_CACHE_PATH = "/home/ray/.cache/huggingface/token"
+
 
 def generate_output_path(output_path_prefix: str, model_id: str) -> str:
     """
@@ -36,3 +41,35 @@ def get_a10g_or_equivalent_accelerator_type() -> str:
     instead.
     """
     return "L4" if _on_gcp_cloud() else "A10G"
+
+
+def prompt_for_hugging_face_token(hf_model: str) -> str:
+    """Prompts the user for Hugging Face token if required by the model.
+    Returns the token as a string. If a token is not required by the model,
+    returns an empty string."""
+
+    try:
+        # Try loading model config to check if Hugging Face token is required.
+        AutoConfig.from_pretrained(hf_model)
+        # If config is loaded successfully, no need for token.
+        return ""
+    except OSError:
+        # Model requires HF token to access. Get the token, either from
+        # cached token or from user input.
+        if not os.path.isfile(HF_TOKEN_CACHE_PATH):
+            print("No cached Hugging Face token found. Starting authentication")
+            # Starts authentication through VSCode overlay.
+            # Token saved to `HF_TOKEN_CACHE_PATH`
+            huggingface_hub.interpreter_login()
+
+        return read_hugging_face_token_from_cache()
+
+
+def read_hugging_face_token_from_cache() -> str:
+    try:
+        with open(HF_TOKEN_CACHE_PATH, "r") as file:
+            return file.read()
+    except FileNotFoundError:
+        raise FileNotFoundError(
+            f"Could not find Hugging Face token cached at {HF_TOKEN_CACHE_PATH}."
+        )
