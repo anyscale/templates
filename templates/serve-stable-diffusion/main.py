f7114538db2cea4bb04de0ced1333cc7ec39bb7b
Huaiwei Sun <scottsun94@gmail.com>
2024-04-29 20:42:08 -0600

    fixup

--- templates/serve-stable-diffusion/main.py
+++ templates/serve-stable-diffusion/main.py
@@ -55,7 +55,7 @@ class APIIngress:
         "num_cpus": 1,
         "accelerator_type": "A10G" # Set accelerator type based on GPU type to use (T4, A10G, L4, V100, A100-40G or A100-80G)
     },
-    max_concurrent_queries=2,   # Maximum number of queries that are sent to a replica of this deployment without receiving a response.
+    max_concurrent_queries=2,  # Maximum number of queries that are sent to a replica of this deployment without receiving a response.
     autoscaling_config={
         "min_replicas": 1,    # The minimum number of model replicas to keep active in the Ray cluster.
         "max_replicas": 99,    # The maximum number of model replicas to keep active in the Ray cluster.
