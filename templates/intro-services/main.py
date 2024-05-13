17610c869e6aab4e5df027e2191e792d812c63a8
Emmy Li <emmy@anyscale.com>
2024-05-13 10:33:36 -0700

    Modify main.py to match example.
    Signed-off-by: Emmy Li <emmy@anyscale.com>

--- templates/intro-services/main.py
+++ templates/intro-services/main.py
@@ -1,16 +1,18 @@
+import logging
 import requests
 from fastapi import FastAPI
 from ray import serve
 
 fastapi = FastAPI()
+logger = logging.getLogger("ray.serve")
 
 @serve.deployment
 @serve.ingress(fastapi)
 class FastAPIDeployment:
-    # FastAPI will automatically parse the HTTP request for us.
-    # Check out https://docs.ray.io/en/latest/serve/http-guide.html
+    # FastAPI automatically parses the HTTP request.
     @fastapi.get("/hello")
     def say_hello(self, name: str) -> str:
+        logger.info("Handling request!")
         return f"Hello {name}!"
 
 my_app = FastAPIDeployment.bind()
