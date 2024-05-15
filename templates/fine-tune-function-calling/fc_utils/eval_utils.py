f29490dc1fb28a42c49d119f3b0fe605362757bb
SumanthRH <sumanthrh@anyscale.com>
2024-05-15 13:18:53 -0700

    cleaned up preprocessing; added pprint and switched to new format
    Signed-off-by: SumanthRH <sumanthrh@anyscale.com>

--- templates/fine-tune-function-calling/fc_utils/eval_utils.py
+++ templates/fine-tune-function-calling/fc_utils/eval_utils.py
@@ -11,31 +11,36 @@ def is_match(response: dict, ground_truth: dict):
     """
     Checks if the response matches the ground truth.
     """
-    if ground_truth["tool_call"] is None:
-        if response["tool_call"] is None:
+    if ground_truth["tool_calls"] is None:
+        if response["tool_calls"] is None:
             if len(ground_truth["content"]):
                 return len(response["content"]) > 0  # non zero content
             return True
         else:  # explicit else for clarity
             return False
 
-    return (
-        response["tool_call"] == ground_truth["tool_call"]
-    )  # makes sure that all fields in the tool call json matches
+    if response["tool_calls"] is None:
+        return False
+    if len(response["tool_calls"]) != len(ground_truth["tool_calls"]):
+        return False
+    for expected_tool_call, actual_tool_call in zip(ground_truth["tool_calls"], response["tool_calls"]):
+        if expected_tool_call != actual_tool_call:
+            return False
 
 
 def parse_and_eval(
     parser: Union[OpenAIResponseParser, AnyscaleResponseParser],
     user_messages,
-    ground_truths,
+    expected_responses,
     tools=None,
 ):
     """
     Parse and eval loop to parse the assistant responses and evaluate them against the ground truth in the conversation.
+    This assumes that an assistant response is expected after every user/tool message.
     Args:
     parser: OpenAIResponseParser or AnyscaleResponseParser object
-    user_messages: list of user  (contains system message and tool responses if any)
-    ground_truths: list of ground truth messages
+    user_messages: list of user messages to send to the chat completion model (contains system message and tool responses if any)
+    expected_responses: list of ground truth responses
     tools: list of tools to available, used by OpenAIResponseParser
     """
     assert user_messages[0]["role"] == "system", "First message must be from system"
@@ -44,6 +49,8 @@ def parse_and_eval(
     for i in range(1, len(user_messages)):
         current_conv.append(user_messages[i])
         if current_conv[-1]["role"] == "tool":
+            # For a tool, we need to have a valid tool call id. This can only be retrieved from the previous assistant response
+            # because our dataset is synthetically constructed
             if (
                 current_conv[-2]["role"] == "assistant"
                 and current_conv[-2]["tool_calls"]
@@ -57,13 +64,30 @@ def parse_and_eval(
             parsed_response = parser.get_parsed_response(current_conv)
         if parsed_response["content"] == ERROR_OUTPUT:
             return None, None  # return None if there's an error
-        match = match and (is_match(parsed_response, ground_truths[i - 1]))
+        match = match and (is_match(parsed_response, expected_responses[i - 1]))
         original_assistant_response = parsed_response["original_response"]
         current_conv.append(dict(original_assistant_response))
         if not match:  # return right away if model output is incorrect
             return current_conv, match
     return current_conv, match
 
+
+def evaluate_gpt4(ds):
+    openai_accuracy = 0
+    pbar = tqdm(total=len(modified_ds), desc="Evaluating GPT4..")
+    for example in ds:
+        openai_messages = example["openai_messages"]
+        openai_tools = [
+            {"type": "function", "function": fn} for fn in example["openai_functions"]
+        ]
+        openai_conv, openai_is_match = parse_and_eval(
+            openai_parser, openai_messages, example["expected_responses"], tools=openai_tools
+        )
+        if openai_conv is None:  # skip if api errors out
+            continue
+        if openai_is_match:
+            openai_accuracy += 1
+    return openai_accuracy
 
 if __name__ == "__main__":
     # Evaluation code in full
