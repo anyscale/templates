5165e966ab83874206c183723dc90088f52b226e
Artur Niederfahrenhorst <artur@anyscale.com>
2024-05-23 10:07:44 -0700

    wi

--- templates/fine-tune-llm_v2/main.py
+++ templates/fine-tune-llm_v2/main.py
@@ -16,22 +16,6 @@ def _get_lora_storage_uri() -> str:
     return f"{artifact_storage}/lora_fine_tuning/"
 
 
-def generate_model_tag(model_id: str) -> str:
-    """
-    Constructs a finetuned model ID based on the Anyscale endpoints convention.
-    """
-    username = os.environ.get("ANYSCALE_USERNAME")
-    if username:
-        username = username.strip().replace(" ", "")[:5]
-        if len(username) < 5:
-            padding_char = username[-1] if username else 'a'
-            username += padding_char * (5 - len(username))
-    else:
-        username = "".join(random.choices(string.ascii_lowercase, k=5))
-    suffix = "".join(random.choices(string.ascii_lowercase, k=5))
-    return f"{model_id}:{username}:{suffix}"
-
-
 def main():
     # Set up the argument parser
     parser = argparse.ArgumentParser(
@@ -53,10 +37,10 @@ def main():
     entrypoint = f"llmforge dev finetune {finetune_config_path}"
 
     if is_lora:
-        model_tag = generate_model_tag(training_config["model_id"])
-        entrypoint += f" --model-tag={model_tag}"
         lora_storage_uri = _get_lora_storage_uri()
         entrypoint += f" --forward-best-checkpoint-remote-uri={lora_storage_uri}"
+        print(f"Note: LoRA weights will also be stored inside {lora_storage_uri}")
+        
     else:
         lora_storage_uri = None
 
@@ -66,10 +50,7 @@ def main():
 
     subprocess.run(entrypoint, check=True, shell=True)
     if lora_storage_uri:
-        print(
-            f"Note: LoRA weights will also be stored in path {lora_storage_uri} under {model_tag} bucket."
-        )
-
+        
 
 if __name__ == "__main__":
     main()
