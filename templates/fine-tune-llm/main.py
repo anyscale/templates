acd41e124fbc411db10fda19ff49d6408ce9b13b
Artur Niederfahrenhorst <artur@anyscale.com>
2024-05-23 12:51:41 -0700

    copy over changes from git push --set-upstream origin copykh/hot-fix-ft-template-model-tag

--- templates/fine-tune-llm/main.py
+++ templates/fine-tune-llm/main.py
@@ -1,7 +1,6 @@
 import argparse
 import os
 import subprocess
-import tempfile
 import yaml
 import random
 import string
@@ -11,15 +10,6 @@ from pathlib import Path
 def _read_yaml_file(file_path):
     with open(file_path, "r") as stream:
         return yaml.safe_load(stream)
-
-
-def get_cld_id() -> str:
-    return os.environ.get("ANYSCALE_CLOUD_ID") or ""
-
-
-def get_region() -> str:
-    return os.environ.get("ANYSCALE_CLOUD_STORAGE_BUCKET_REGION") or ""
-
 
 def _get_lora_storage_uri() -> str:
     artifact_storage = os.environ.get("ANYSCALE_ARTIFACT_STORAGE")
@@ -46,10 +36,7 @@ def generate_model_tag(model_id: str) -> str:
 def main():
     # Set up the argument parser
     parser = argparse.ArgumentParser(
-        description="Submit a job with configuration files"
-    )
-    parser.add_argument(
-        "job_config", type=str, help="Path to the job configuration YAML file"
+        description="Submit a job with a configuration file"
     )
     parser.add_argument(
         "finetune_config",
@@ -57,58 +44,53 @@ def main():
         help="Path to the fine-tuning configuration YAML file",
     )
 
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="./outputs",
+        help="Path to store the configs before job submission"
+    )
+
     # Parse arguments
     args = parser.parse_args()
 
-    job_config_path = args.job_config
     finetune_config_path = args.finetune_config
-
-    job_config = _read_yaml_file(job_config_path)
     training_config = _read_yaml_file(finetune_config_path)
 
-    cld_id = get_cld_id()
-    region = get_region()
-    job_config["compute_config"]["cloud_id"] = cld_id
-    job_config["compute_config"]["region"] = region
-
     is_lora = "lora_config" in training_config
-    entrypoint = f"llmforge dev finetune {finetune_config_path}"
 
     if is_lora:
         model_tag = generate_model_tag(training_config["model_id"])
-        entrypoint += f" --model-tag={model_tag}"
         lora_storage_uri = _get_lora_storage_uri()
-        entrypoint += f" --forward-best-checkpoint-remote-uri={lora_storage_uri}"
+        # Required for registering the model on Anyscale.
+        training_config.update({
+            "forward_checkpoint_config": {
+                "tag": model_tag,
+                "remote_uri": lora_storage_uri
+            }
+        })
     else:
-        lora_storage_uri = None
+        model_tag, lora_storage_uri = None, None
 
-    job_config["entrypoint"] = entrypoint
-    job_config["name"] = Path(finetune_config_path).stem
 
-    api_key = os.environ.get("WANDB_API_KEY")
+    output_dir = Path(args.output_dir)
+    output_dir.mkdir(exist_ok=True, parents=True)
+    llmforge_config_path = output_dir / Path(finetune_config_path).name
+    entrypoint = f"llmforge dev finetune {llmforge_config_path}"
+
+
+    with open(llmforge_config_path, "w") as f:
+        yaml.safe_dump(training_config, f)
+
+    api_key = os.environ.get("WANDB_API_KEY", "")
     if api_key:
-        job_config.setdefault("runtime_env", {}).setdefault("env_vars", {})[
-            "WANDB_API_KEY"
-        ] = api_key
-        job_config["runtime_env"]["env_vars"]["RAY_OVERRIDE_JOB_RUNTIME_ENV"] = "1"
+        entrypoint = f"WANDB_API_KEY={api_key} {entrypoint}"
 
-    with tempfile.NamedTemporaryFile(
-        mode="w+", delete=False, dir=".", suffix=".yaml"
-    ) as temp_file:
-        yaml.safe_dump(job_config, temp_file)
-        temp_file_name = temp_file.name
-
-    # Call `anyscale job submit` on the temporary YAML file
-    try:
-        subprocess.run(["anyscale", "job", "submit", temp_file_name], check=True)
-        if lora_storage_uri:
-            print(
-                f"Note: LoRA weights will also be stored in path {lora_storage_uri} under {model_tag} bucket."
-            )
-    finally:
-        # Clean up by deleting the temporary file
-        os.remove(temp_file_name)
-        pass
+    subprocess.run(entrypoint, check=True, shell=True)
+    if lora_storage_uri:
+        print(
+            f"Note: LoRA weights will also be stored in path {lora_storage_uri} under {model_tag} bucket."
+        )
 
 
 if __name__ == "__main__":
