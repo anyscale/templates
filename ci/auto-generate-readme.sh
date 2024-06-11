7365668bfb5a50286f88248d8aa744db8c011f14
GokuMohandas <gokumd@gmail.com>
2024-06-11 10:19:14 -0700

    added e2e llm workflows template and exception to README.md generation

--- ci/auto-generate-readme.sh
+++ ci/auto-generate-readme.sh
@@ -6,7 +6,7 @@ notebook_files=$(find ../templates -name "README.ipynb")
 
 # Loop through each notebook file
 for notebook_file in $notebook_files; do
-    if [ $notebook_file != "../templates/templates/getting-started/README.ipynb" ] && ! grep -q "Time to complete" $notebook_file; then
+    if [ $notebook_file != "../templates/templates/getting-started/README.ipynb" ] && [ "$notebook_file" != "../templates/templates/e2e-llm-workflows/README.ipynb" ] && ! grep -q "Time to complete" $notebook_file; then
         echo "**********"
         echo "LINT ERROR: $notebook_file must include 'Time to complete' statement, failing."
         echo "**********"
