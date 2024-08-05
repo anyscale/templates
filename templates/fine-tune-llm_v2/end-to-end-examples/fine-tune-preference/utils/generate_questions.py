dfb224c4ae4d93a0ec302057cd5c26d53a6f43ee
Sumanth R Hegde <39546518+SumanthRH@users.noreply.github.com>
2024-08-04 18:42:31 -0700

    Minor improvements to DPO code (#281)
    # What does this PR do?
    
    Minor improvements to DPO code (added argparse + improved string
    handling). More coming.
    
    Used `black` and `isort` with default parameters to format all the
    files. We should just standardize on this for now since `templates`
    doesn't have the relevant pre commit hooks.
    
    ---------
    
    Signed-off-by: SumanthRH <sumanthrh@anyscale.com>

--- templates/fine-tune-llm_v2/end-to-end-examples/fine-tune-preference/utils/generate_questions.py
+++ templates/fine-tune-llm_v2/end-to-end-examples/fine-tune-preference/utils/generate_questions.py
@@ -1,9 +1,16 @@
-import ray
+import argparse
+import logging
 import os
 
-from transformers import AutoTokenizer
-from synthetic_data_utils import LLMPredictor, format_into_prompt_rawtext, shuffle_qa
 import datasets
+import ray
+from transformers import AutoTokenizer
+
+from utils.synthetic_data_utils import (LLMPredictor,
+                                        format_into_prompt_rawtext, shuffle_qa)
+from utils.utils import init_logger
+
+logger = init_logger()
 
 PROMPT_TEMPLATE_QUESTION_GENERATION = """Given the following text, generate five multiple choice questions with the following format. The questions must be simple enough to be answerable with only general important details that would be included in a short two sentence summary of the text. The questions must be only answerable when given the text and should not be answerable with common knowledge. Do not write questions about minute details in the text, only the most important points.
 
@@ -32,75 +39,116 @@ Text:
 {article}"""
 
 OUTPUT_PATH = f"{os.environ.get('ANYSCALE_ARTIFACT_STORAGE')}/preference_tuning_summarization_example/qa_annotations"
-OUTPUT_PATH_TRAIN = OUTPUT_PATH + "_train"
-OUTPUT_PATH_TEST = OUTPUT_PATH + "_test"
+OUTPUT_PATH_TRAIN = f"{OUTPUT_PATH}_train"
+OUTPUT_PATH_TEST = f"{OUTPUT_PATH}_test"
+# New output columns
+OUTPUT_QUESTION_FIELD = "qa_generation_questions"
+OUTPUT_ANSWER_FIELD = "qa_generation_answers"
+OUTPUT_RAW_GENERATION_FIELD = "qa_generation_raw_model_output"
 
-HF_MODEL = "meta-llama/Meta-Llama-3-70B"
-# The number of LLM instances to use.
-NUM_LLM_INSTANCES = 2
-# The number of GPUs to use per LLM instance. NUM_GPUS_PER_INSTANCE > 1 will use tensor parallelism across all GPUs.
-NUM_GPUS_PER_INSTANCE = 4
-# The type of GPU to use
-GPU_TYPE = "H100" # use either H100 or A100 instances here
-# Batch size for each instance
-BATCH_SIZE = 16
-
-# Initialize Ray with a Runtime Environment.
-ray.init(
-    runtime_env={
-        "env_vars": {"HF_TOKEN": os.environ["HF_TOKEN"], "HF_HOME": "/mnt/local_storage/.cache/huggingface"},
-    }
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--model-id",
+    type=str,
+    default="meta-llama/Meta-Llama-3-70B",
+    help="Model to use for generation",
+)
+parser.add_argument(
+    "--concurrency",
+    type=int,
+    required=True,
+    help="Number of LLM instances to use concurrently",
+)
+parser.add_argument(
+    "--num-gpus-per-instance",
+    type=int,
+    required=True,
+    help="Number of GPUs to use per instance. This also sets the tensor parallelism size with vLLM",
+)
+parser.add_argument(
+    "--gpu-type",
+    type=str,
+    default="H100",
+    help="Accelerator type to use. Recommended to be H100 or A100",
+)
+parser.add_argument(
+    "--batch-size", type=int, required=True, help="Batch size for each instance"
+)
+parser.add_argument(
+    "--num-data-blocks-per-device",
+    type=int,
+    default=1,
+    help="Number of Ray data blocks per GPU device. If unsure, use the default value",
 )
 
-hf_ds = datasets.load_dataset("abisee/cnn_dailymail", '3.0.0', split="train").shuffle(seed=21)
-# extract a subset of 21000 articles, (20000 for train, 1000 for testing)
-hf_ds_subset = hf_ds.select(range(100))
-# the resulting keys for the dataset are "article" (which contains the text) and "id" only
-hf_ds_subset.remove_columns(["highlights"])
+if __name__ == "__main__":
+    args = parser.parse_args()
 
-ds = ray.data.from_huggingface(hf_ds_subset)
-ds = ds.repartition(NUM_LLM_INSTANCES * 8)
+    # Initialize Ray with a Runtime Environment.
+    ray.init(
+        runtime_env={
+            "env_vars": {
+                "HF_TOKEN": os.environ["HF_TOKEN"],
+                "HF_HOME": "/mnt/local_storage/.cache/huggingface",
+            },
+        },
+        logging_config=ray.LoggingConfig(encoding="JSON", log_level="INFO"),
+    )
 
-ds = ds.map(
-    format_into_prompt_rawtext,
-    fn_kwargs=dict(
-        template=PROMPT_TEMPLATE_QUESTION_GENERATION,
-        tokenizer=AutoTokenizer.from_pretrained(HF_MODEL),
-        col_name="qa_generation_prompt"
-    ),
-    num_cpus=0
-)
+    hf_ds = datasets.load_dataset("abisee/cnn_dailymail", "3.0.0", split="train").shuffle(
+        seed=21
+    )
+    # the resulting keys for the dataset are "article" (which contains the text) and "id" only
+    hf_ds.remove_columns(["highlights"])
 
-ds = ds.map_batches(
-    LLMPredictor,
-    fn_constructor_kwargs=dict(
-        model_location=HF_MODEL,
-        col_in="qa_generation_prompt",
-        col_out="qa_generation_raw_model_output",
-        temperature=0,
-        max_tokens=4096,
-        vllm_settings=dict(
-            tensor_parallel_size=NUM_GPUS_PER_INSTANCE,
-            max_model_len=8192,
-        )
-    ),
-    concurrency=NUM_LLM_INSTANCES,
-    num_gpus=NUM_GPUS_PER_INSTANCE,
-    accelerator_type=GPU_TYPE,
-    batch_size=BATCH_SIZE,
-)
+    ds = ray.data.from_huggingface(hf_ds)
+    # By default, a HF dataset is converted to a Materialized dataset and the number of blocks can be low
+    num_blocks = (
+        args.concurrency * args.num_gpus_per_instance * args.num_data_blocks_per_device
+    )
+    ds = ds.repartition(num_blocks)
 
-ds = ds.map(
-    shuffle_qa,
-    fn_kwargs=dict(
-        col_in="qa_generation_raw_model_output",
-        col_out_prompt="qa_generation_questions",
-        col_out_answers="qa_generation_answers",
-    ),
-    num_cpus=0
-)
+    prompt_field = "qa_generation_prompt"
+    ds = ds.map(
+        format_into_prompt_rawtext,
+        fn_kwargs=dict(
+            template=PROMPT_TEMPLATE_QUESTION_GENERATION,
+            tokenizer=AutoTokenizer.from_pretrained(args.model_id),
+            col_name=prompt_field,
+        ),
+        num_cpus=0,
+    )
+    ds = ds.map_batches(
+        LLMPredictor,
+        fn_constructor_kwargs=dict(
+            model_location=args.model_id,
+            col_in=prompt_field,
+            col_out=OUTPUT_RAW_GENERATION_FIELD,
+            temperature=0,
+            max_tokens=4096,
+            vllm_settings=dict(
+                tensor_parallel_size=args.num_gpus_per_instance,
+                max_model_len=8192,
+                dtype="bfloat16",
+            ),
+        ),
+        concurrency=args.concurrency,
+        num_gpus=args.num_gpus_per_instance,
+        accelerator_type=args.gpu_type,
+        batch_size=args.batch_size,
+    )
 
-train_ds, test_ds = ds.train_test_split(test_size=0.05)
+    ds = ds.map(
+        shuffle_qa,
+        fn_kwargs=dict(
+            col_in=OUTPUT_RAW_GENERATION_FIELD,
+            col_out_prompt=OUTPUT_QUESTION_FIELD,
+            col_out_answers=OUTPUT_ANSWER_FIELD,
+        ),
+        num_cpus=0,
+    )
 
-train_ds.write_parquet(OUTPUT_PATH_TRAIN)
-test_ds.write_parquet(OUTPUT_PATH_TEST)
+    train_ds, test_ds = ds.train_test_split(test_size=0.05)
+
+    train_ds.write_parquet(OUTPUT_PATH_TRAIN)
+    test_ds.write_parquet(OUTPUT_PATH_TEST)
--- templates/fine-tune-llm_v2/end-to-end-examples/fine-tune-preference/utils/utils.py
+++ templates/fine-tune-llm_v2/end-to-end-examples/fine-tune-preference/utils/utils.py
@@ -1,13 +1,19 @@
+import logging
 import os
 import random
-import requests
 import string
 
 import huggingface_hub
+import requests
 from transformers import AutoConfig
 
 HF_TOKEN_CACHE_PATH = "/mnt/local_storage/data/cache/huggingface/token"
 HF_TOKEN_LOCAL_PATH = "huggingface_token.txt"
+
+
+def init_logger():
+    """Get the root logger"""
+    return logging.getLogger()
 
 
 def generate_output_path(output_path_prefix: str, model_id: str) -> str:
@@ -18,67 +24,6 @@ def generate_output_path(output_path_prefix: str, model_id: str) -> str:
     if username:
         username = username[:5]
     else:
-        username = "".join(
-            random.choices(string.ascii_lowercase, k=5)
-        )
+        username = "".join(random.choices(string.ascii_lowercase, k=5))
     suffix = "".join(random.choices(string.ascii_lowercase, k=5))
     return f"{output_path_prefix}/{model_id}:{username}:{suffix}"
-
-
-def _on_gcp_cloud() -> bool:
-    """Detects if the cluster is running on GCP."""
-    try:
-        resp = requests.get("http://metadata.google.internal")
-    except:  # noqa: E722
-        return False
-    return resp.headers["Metadata-Flavor"] == "Google"
-
-
-def get_a10g_or_equivalent_accelerator_type() -> str:
-    """Returns an accelerator type string for an A10G (or equivalent) GPU.
-
-    Equivalence is determined by the amount of GPU memory in this case.
-    GCP doesn't provide instance types with A10G GPUs, so we request L4 GPUs
-    instead.
-    """
-    return "L4" if _on_gcp_cloud() else "A10G"
-
-
-def prompt_for_hugging_face_token(hf_model: str) -> str:
-    """Prompts the user for Hugging Face token if required by the model.
-    Returns the token as a string. If a token is not required by the model,
-    returns an empty string."""
-
-    url = f"https://huggingface.co/api/models/{hf_model}"
-
-    response = requests.get(url)
-    if response.status_code == 200:
-        return ""
-    elif response.status_code == 401:
-        if os.path.isfile(HF_TOKEN_LOCAL_PATH):
-            return read_hugging_face_token_from_cache(HF_TOKEN_LOCAL_PATH)
-        if not os.path.isfile(HF_TOKEN_CACHE_PATH):
-            print("No cached Hugging Face token found. Starting authentication on VS Code overlay...")
-            # Starts authentication through VSCode overlay.
-            # Token saved to `HF_TOKEN_CACHE_PATH`
-            huggingface_hub.interpreter_login()
-
-        tkn = read_hugging_face_token_from_cache(HF_TOKEN_CACHE_PATH)
-        # Write the token to a local file, so it can be used by Ray job.
-        with open(HF_TOKEN_LOCAL_PATH, "w") as file:
-            file.write(tkn)
-        return tkn
-    else:
-        raise Exception(f"Failed to access the model. Status code: {response.status_code}")
-
-
-def read_hugging_face_token_from_cache(path) -> str:
-    try:
-        with open(path, "r") as file:
-            tkn = file.read()
-        print(f"Successfully read cached token at {path}.")
-        return tkn
-    except FileNotFoundError:
-        raise FileNotFoundError(
-            f"Could not find Hugging Face token cached at {path}."
-        )
