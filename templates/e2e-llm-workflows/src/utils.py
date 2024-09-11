be0374288f0ef817561433b542fc92c5a470e035
SumanthRH <sumanthrh@anyscale.com>
2024-09-10 23:14:21 -0700

    update
    Signed-off-by: SumanthRH <sumanthrh@anyscale.com>

--- templates/e2e-llm-workflows/src/utils.py
+++ templates/e2e-llm-workflows/src/utils.py
@@ -2,10 +2,17 @@ from contextlib import contextmanager
 import os
 from tempfile import TemporaryDirectory
 import boto3
+from urllib.parse import urlparse
 from ray.data import Dataset
 
 
-def download_files_from_bucket(bucket, path, local_dir):
+def download_files_from_s3(s3_uri, local_dir):
+    parsed_uri = urlparse(s3_uri)
+    if parsed_uri.scheme != "s3":
+        raise ValueError(f"Expected S3 URI, got {s3_uri}")
+    bucket = parsed_uri.netloc
+    path = parsed_uri.path.lstrip("/")
+
     s3 = boto3.client('s3')
     paginator = s3.get_paginator('list_objects_v2')
     page_iterator = paginator.paginate(Bucket=bucket, Prefix=path)
