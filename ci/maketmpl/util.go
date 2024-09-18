3d28a8b59d15f1e051f452472c96bb66f9105dfe
Lonnie Liu <lonnie@anyscale.com>
2024-09-18 12:52:38 +0000

    [maketmpl] add util function and test
    used in building template/app zip files

---
+++ ci/maketmpl/util.go
@@ -0,0 +1,36 @@
+package maketmpl
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"os"
+)
+
+func checkIsDir(path string) error {
+	stat, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+	if !stat.IsDir() {
+		return fmt.Errorf("%s is not a directory", path)
+	}
+	return nil
+}
+
+func addFileToZip(z *zip.Writer, file, pathInZip string) error {
+	f, err := os.Open(file)
+	if err != nil {
+		return fmt.Errorf("open file %q: %w", file, err)
+	}
+	defer f.Close()
+
+	w, err := z.Create(pathInZip)
+	if err != nil {
+		return fmt.Errorf("create file in zip: %w", err)
+	}
+	if _, err := io.Copy(w, f); err != nil {
+		return fmt.Errorf("copy file to zip: %w", err)
+	}
+	return nil
+}
