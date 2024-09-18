22b37a7bab233dab58a5561963cf63931bef50be
Lonnie Liu <lonnie@anyscale.com>
2024-09-18 04:09:40 +0000

    .

--- ci/maketmpl/builder.go
+++ ci/maketmpl/builder.go
@@ -3,7 +3,6 @@ package maketmpl
 import (
 	"archive/zip"
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -37,34 +36,6 @@ func newBuilder(t *Template, baseDir string) *builder {
 	tmplDir := filepath.Join(baseDir, t.Dir)
 
 	return &builder{tmpl: t, baseDir: baseDir, tmplDir: tmplDir}
-}
-
-func checkIsDir(path string) error {
-	fi, err := os.Stat(path)
-	if err != nil {
-		return err
-	}
-	if !fi.IsDir() {
-		return fmt.Errorf("%s is not a directory", path)
-	}
-	return nil
-}
-
-func addFileToZip(z *zip.Writer, file, path string) error {
-	f, err := os.Open(file)
-	if err != nil {
-		return fmt.Errorf("open file %q: %w", file, err)
-	}
-	defer f.Close()
-
-	w, err := z.Create(path)
-	if err != nil {
-		return fmt.Errorf("create file in zip: %w", err)
-	}
-	if _, err := io.Copy(w, f); err != nil {
-		return fmt.Errorf("copy file to zip: %w", err)
-	}
-	return nil
 }
 
 func (b *builder) listFiles() ([]string, error) {
