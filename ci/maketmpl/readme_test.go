8a17b691cd284e4e38057f5dd3f68b1f4917e177
Lonnie Liu <lonnie@anyscale.com>
2024-09-24 20:30:06 +0000

    add release

--- ci/maketmpl/readme_test.go
+++ ci/maketmpl/readme_test.go
@@ -20,7 +20,6 @@ func TestReadmeFile_writeGitHubMD(t *testing.T) {
 	}, "\n")
 
 	path := filepath.Join(tmp, "readme.md")
-
 	if err := os.WriteFile(path, []byte(content), 0600); err != nil {
 		t.Fatal("write readme: ", err)
 	}
@@ -55,5 +54,54 @@ func TestReadmeFile_writeGitHubMD(t *testing.T) {
 }
 
 func TestReadmeFile_writeReleaseMD(t *testing.T) {
+	tmp := t.TempDir()
 
+	content := strings.Join([]string{
+		"# example",
+		"",
+		`![img1](img1.png)<img src='img2.png' width=400px />extra text`,
+	}, "\n")
+
+	for _, file := range []struct {
+		name, content string
+	}{
+		{"img1.png", "img1"},
+		{"img2.png", "img2"},
+		{"readme.md", content},
+	} {
+		path := filepath.Join(tmp, file.name)
+		if err := os.WriteFile(path, []byte(file.content), 0600); err != nil {
+			t.Fatalf("write %q: %s", file.name, err)
+		}
+	}
+
+	input := filepath.Join(tmp, "readme.md")
+	readme, err := readReadmeFile(input)
+	if err != nil {
+		t.Fatal("read readme: ", err)
+	}
+
+	output := filepath.Join(tmp, "readme.release.md")
+	if err := readme.writeReleaseMD(output, tmp); err != nil {
+		t.Fatal("write release md: ", err)
+	}
+
+	got, err := os.ReadFile(output)
+	if err != nil {
+		t.Fatal("read output: ", err)
+	}
+
+	want := strings.Join([]string{
+		"# example",
+		"",
+		strings.Join([]string{
+			`<img src="data:image/png;base64,aW1nMQ==" alt="img1" />`,
+			`<img src="data:image/png;base64,aW1nMg==" style="width: 400px" />`,
+			`extra text`,
+		}, ""),
+	}, "\n")
+
+	if string(got) != want {
+		t.Errorf("got:\n---\n%s\n---\nwant:\n---\n%s\n---\n", got, want)
+	}
 }
