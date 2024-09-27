530888abd61f65d143f0ad18d8e08be767aedb70
Rob de Wit <RCdeWit@users.noreply.github.com>
2024-09-27 12:39:31 -0700

    Remove some friction from e2e LLM template (#360)
    - Remove potentially breaking `pip install -U anyscale`, since it's no
    longer needed.
    - Automatically parse in the S3 bucket locations to the train config
    yaml
    
    ---------
    
    Co-authored-by: Sumanth R Hegde <39546518+SumanthRH@users.noreply.github.com>
    Co-authored-by: GokuMohandas <gokumd@gmail.com>

--- templates/e2e-llm-workflows/src/utils.py
+++ templates/e2e-llm-workflows/src/utils.py
@@ -5,6 +5,7 @@ from tempfile import TemporaryDirectory
 import boto3
 from urllib.parse import urlparse
 from ray.data import Dataset
+import yaml
 
 CLUSTER_STORAGE_PATH = "/mnt/cluster_storage"
 
@@ -64,3 +65,13 @@ def get_dataset_file_path(dataset: Dataset):
         assert len(os.listdir(temp_path)) == 1, "The dataset should be written to a single file"
         dataset_file_path = f"{temp_path}/{os.listdir(temp_path)[0]}"
         yield dataset_file_path
+
+def set_key_value_in_config_file(config_path: str, key: str, value: str):
+    """Take a path to a .json config file and update the key to a value, then save the file"""
+    with open(config_path, 'r') as stream:
+        loaded = yaml.safe_load(stream)
+    # Modify the fields from the dict
+    loaded[key] = value
+    # Save it again
+    with open(config_path, 'w') as stream:
+        yaml.dump(loaded, stream, default_flow_style=False)
