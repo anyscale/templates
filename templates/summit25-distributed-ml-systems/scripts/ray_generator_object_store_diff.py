693dffc36bc6d97001bd04d3d24908fdf0d3c5d0
Marwan Sarieddine <sarieddine.marwan@gmail.com>
2025-10-31 11:52:53 -0700

    cleaner output from script
    Signed-off-by: Marwan Sarieddine <marwan@anyscale.com>

--- templates/summit25-distributed-ml-systems/scripts/ray_generator_object_store_diff.py
+++ templates/summit25-distributed-ml-systems/scripts/ray_generator_object_store_diff.py
@@ -8,17 +8,19 @@ import gc
 
 def list_objects():
     """List and print Ray objects with their sizes in MiB."""
-    for obj_state in ray.util.state.list_objects(raise_on_missing_output=False):
+    for obj_state in ray.util.state.list_objects(
+        filters=[("TASK_STATUS", "!=", "NIL")],
+        raise_on_missing_output=False
+    ):
         if obj_state.object_size > 1000:
             print(
                 f"{obj_state.object_id.replace('f', '')}: {obj_state.object_size / 1024**2:.2f} MiB"
                 f", REF TYPE: {obj_state.reference_type}"
-                f", CALL SITE: {obj_state.call_site.split('/')[-1]}"
             )
     print("-" * 100)
 
 
-@ray.remote
+@ray.remote(num_returns="streaming")
 def generate_blocks(num_blocks=10, block_size_mb=100):
     """Generate data blocks and yield them one by one."""
     for i in range(num_blocks):
@@ -57,10 +59,13 @@ if __name__ == "__main__":
     ray.init()
 
     # Generate, build, and transform data blocks
-    print("Inspecting the impact of a Ray generator task on the object store")
+    print("\nInspecting the impact of a Ray generator task on the object store")
     for obj_ref in generate_blocks.remote():
         transform_block.remote(obj_ref)
+    
+    # clean up last reference
+    del obj_ref
+    gc.collect()
 
-    print("Inspecting the impact of a Ray non-generator task on the object store")
+    print("\nInspecting the impact of a Ray non-generator task on the object store")
     ray.get(transform_all_data.remote(build_all_data.remote()))
-    gc.collect()  # Trigger garbage collection
