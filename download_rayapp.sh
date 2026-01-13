fe34f52aaa8c8e1ee17b0fe7b469af465825efed
elliot-barn <elliot.barnwell@anyscale.com>
2026-01-13 23:53:09 +0000

    creating local directory
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- download_rayapp.sh
+++ download_rayapp.sh
@@ -4,4 +4,7 @@ set -euo pipefail
 
 wget https://github.com/ray-project/rayci/releases/download/v0.25.0/rayapp-linux-amd64
 chmod +x rayapp-linux-amd64
-mv rayapp-linux-amd64 "$HOME/.local/bin/rayapp" && echo "Rayapp downloaded and installed"
+mkdir -p "$HOME/.local/bin"
+mv rayapp-linux-amd64 "$HOME/.local/bin/rayapp"
+echo "$HOME/.local/bin" >> "$GITHUB_PATH"
+echo "Successfully downloaded rayapp"
