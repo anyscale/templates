ee9f446c8cdf3bab970acd190e5a180a8d65336d
elliot-barn <elliot.barnwell@anyscale.com>
2026-01-13 23:54:39 +0000

    using rayapp in path
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- download_rayapp.sh
+++ download_rayapp.sh
@@ -4,7 +4,5 @@ set -euo pipefail
 
 wget https://github.com/ray-project/rayci/releases/download/v0.25.0/rayapp-linux-amd64
 chmod +x rayapp-linux-amd64
-mkdir -p "$HOME/.local/bin"
-mv rayapp-linux-amd64 "$HOME/.local/bin/rayapp"
-echo "$HOME/.local/bin" >> "$GITHUB_PATH"
+mv rayapp-linux-amd64 rayapp
 echo "Successfully downloaded rayapp"
