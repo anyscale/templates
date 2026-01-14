08b03cf477e7da41db30e329c40a246e28fc5d74
Elliot Barnwell <elliot.barnwell@anyscale.com>
2026-01-13 17:27:18 -0800

    replacing maketmpl with rayapp (#508)
    adding download rayapp script (downloaded from rayci release artifact)
    https://github.com/ray-project/rayci/releases/v0.25.0
    building templates using rayapp in github actions
    
    ---------
    
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

---
+++ download_rayapp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -euo pipefail
+
+wget https://github.com/ray-project/rayci/releases/download/v0.25.0/rayapp-linux-amd64
+chmod +x rayapp-linux-amd64
+mv rayapp-linux-amd64 rayapp
+echo "Successfully downloaded rayapp"
