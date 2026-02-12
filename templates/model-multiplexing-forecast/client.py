9c1d1118fd8da53ed742893d384eb6be75690db7
elliot-barn <elliot.barnwell@anyscale.com>
2026-02-12 01:22:03 +0000

    adding comment for testing
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- templates/model-multiplexing-forecast/client.py
+++ templates/model-multiplexing-forecast/client.py
@@ -4,6 +4,8 @@ import requests
 # time series data
 sequence_data = [100, 102, 98, 105, 110, 108, 112, 115, 118, 120]
 
+# Elliot was here
+
 # Send request with customer_id in header
 response = requests.post(
     "http://localhost:8000",
