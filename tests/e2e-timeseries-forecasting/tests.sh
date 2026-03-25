de87b586a506292e91e4f4318da9bc260a0b8ae0
elliot-barn <elliot.barnwell@anyscale.com>
2026-03-25 00:07:18 +0000

    updating tests.sh
    Signed-off-by: elliot-barn <elliot.barnwell@anyscale.com>

--- tests/e2e-timeseries-forecasting/tests.sh
+++ tests/e2e-timeseries-forecasting/tests.sh
@@ -2,4 +2,6 @@
 
 set -euxo pipefail
 
+pip install -r requirements.txt
+
 jupyter execute e2e_timeseries/01-Distributed-Training.ipynb e2e_timeseries/02-Validation.ipynb e2e_timeseries/03-Serving.ipynb
