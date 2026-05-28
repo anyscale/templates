1fce110efe9df9677fe08cbc768256c7fecd0c60
Aydin Abiar <aydin@anyscale.com>
2026-05-27 20:25:17 -0700

    fix(tests): pin nbconvert==7.16.6 (lxml.html.clean ImportError)
    templateexporter.py:25 imports lxml.html.clean eagerly, which lxml >=5.2
    removed. Unpinned 'pip install nbconvert' kept the broken pre-installed
    version. 7.16.6 doesn't have the eager import.
    
    Signed-off-by: Aydin Abiar <aydin@anyscale.com>

--- tests/getting-started/tests.sh
+++ tests/getting-started/tests.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 # Intro notebook is a CI-safe Ray hello-world; run it end-to-end.
-pip install -q papermill nbconvert ipykernel
+pip install -q papermill nbconvert==7.16.6 ipykernel
 jupyter nbconvert --to notebook README.ipynb \
   --TagRemovePreprocessor.enabled=True \
   --TagRemovePreprocessor.remove_cell_tags=skip-in-ci \
